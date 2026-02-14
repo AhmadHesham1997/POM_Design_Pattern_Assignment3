df65e6379aa0f349f3f1cc230f176889da12488f
AhmadHesham1997 <a.hesham2014@gmail.com>
2026-02-14 22:03:59 +0200

    Commit

---
+++ src/test/java/F01_Login.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+// Its preferd to put each mehthod that goes to a seperate page in a seprate class file
+// Page Object Model POM design pattern
+// F01_Login is the test class
+// P01_LoginPage is the page class
+public class F01_Login {
+    WebDriver driver;
+    P01_LoginPage loginPage;
+
+    @BeforeMethod
+    public void setUp() {
+        // Initialize ChromeDriver
+        driver = new ChromeDriver();
+        // Navigate to the main website
+        driver.get("https://the-internet.herokuapp.com");
+        // Initialize the login page object
+        loginPage = new P01_LoginPage(driver);
+        // Navigate to the form authentication page
+        loginPage.navigateToFormAuthenticationPage();
+    }
+
+    @Test
+    public void TC_A() {
+        // Positive test case
+        loginPage.loginTest("tomsmith", "SuperSecretPassword!");
+        Assert.assertTrue(loginPage.assertValidLogin(), "Login was not successful!");
+    }
+    @Test
+    public void TC_B() {
+        // Negative test case
+        loginPage.loginTest("invalidUser", "SuperSecretPassword!");
+        Assert.assertTrue(loginPage.assertInvalidLogin(), "Invalid login was not handled correctly!");
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        // Close the browser
+        driver.quit();
+    }
+}
---
+++ src/test/java/P01_LoginPage.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class P01_LoginPage {
+    WebDriver driver1;
+    //locators
+    final private By usernameField = By.id("username");
+    final private By passwordField = By.id("password");
+    final private By loginButton = By.className("fa-sign-in");
+    final private By successMessage = By.id("flash");
+    //getting form authentication link by xpath using link text
+    final private By formAuthenticationLink = By.xpath("//a[text()='Form Authentication']");
+    //constructor
+    public P01_LoginPage(WebDriver driver) {
+        this.driver1 = driver;
+    }
+
+    //methods
+    //method to navigate to form authentication page
+    //this method should be in a separate class called P00_HomePage but for simplicity we are keeping it here
+    void navigateToFormAuthenticationPage() {
+        driver1.findElement(formAuthenticationLink).click();
+    }
+    //method to perform login action
+    //this method should be in a separate class called P02_LoginPage but for simplicity we are keeping it here
+    void loginTest(String username, String password) {
+        //enter username and password and click login button
+        driver1.findElement(usernameField).sendKeys(username);
+        driver1.findElement(passwordField).sendKeys(password);
+        driver1.findElement(loginButton).click();
+    }
+
+    //methods for assertions
+    //positive scenario
+    //this method should be in a separate class called P03_SecureAreaPage but for simplicity we are keeping it here
+    boolean assertValidLogin(){
+        return driver1.findElement(successMessage).getText().contains("You logged into a secure area!");
+    }
+    //negative scenario
+    //this method should be in a separate class called P02_LoginPage but for simplicity we are keeping it here
+    boolean assertInvalidLogin(){
+        return driver1.findElement(successMessage).getText().contains("Your username is invalid!");
+    }
+}
